29b8ca7290670623ed0a2b8f3e19a5dc52f188cb
Michael Chen <michaelcchen@berkeley.edu>
2016-02-03 01:17:20 -0800

    done

--- hw1.playground/Contents.swift
+++ hw1.playground/Contents.swift
@@ -22,33 +22,34 @@ class Words {
 //: ### variables the same type? If not, why?
 
 
-//: [EXPLAIN YOUR ANSWER HERE]
+//: both values are of type String optional. The only difference is that the instance values are forcibly unwrapped. Thus, if wordA or wordB is nil, there will be a runtime exception.
 
 
 //: ## Q2: Variable Types and Function Types
-    func arePalindromes(words: [String]) -> Bool {
+    static func arePalindromes(words: [String]) -> Bool {
         let reversedWords = words.map() {String($0.characters.reverse())}
-        var numElements = words.count
+        let numElements = words.count
         
-        for let i = 0; i < numElements; i++ {
+        for var i = 0; i < numElements; i++ {
             if words[i] != reversedWords[i] {
                 return false
             }
         }
+        return true
     }
 //: ### Why does the compiler dislike the **for loop**? Fix it.
 //: ### What else is wrong with this function? You may have to refer to (but **not**
 //: ### change) the code at the very bottom. Debug the function.
 
 
-//: [EXPLAIN YOUR ANSWER HERE]
+//: The compiler dislikes the for loop because is is declared with the "let" keyword, which implies that i is a constant. Thus, i cannot be incremented using "i++". We can fix it by changing "let" to "var." Furthermore, numElements can be declared as "let" instead of as "var" as the value never changes. The function can be declared as static because it's being called by instances of another class. Finally, the function needs to return true at the very end to fulfill the function return type (boolean - it needs both true and false cases depending on if the inputs are palindromes or not).
 
 
 //: ## Q3: More Functions and Object Initialization
-    class func isAnagram() -> Bool {
-        var countLetters : [Character : Int] //Line X
-        var lenA = self.wordA.characters.count
-        var lenB = self.wordB.characters.count
+    func isAnagram() -> Bool {
+        var countLetters = [Character : Int]() //Line X
+        let lenA = self.wordA.characters.count
+        let lenB = self.wordB.characters.count
         
         if lenA != lenB {
             return false
@@ -75,13 +76,13 @@ class Words {
             }
         }
         
-        for (letter, count) in countLetters {
+        for (_, count) in countLetters {
             if count != 0 {
                 return false
             }
         }
         
-        return nil
+        return true
     }
 //: ### What is the problem with declaring **countLetters** as we do in **Line X**,
 //: ### and then using it in **Line Y**? Fix it (by only changing **Line X**).
@@ -89,7 +90,7 @@ class Words {
 //: ### change) the code at the very bottom. Debug the function.
 
 
-//: [EXPLAIN YOUR ANSWER HERE]
+//: To declare countLetters, the colon needs to be replaced by equals sign and the parenthesises are required for initailization. Furthermore, the the class declaration needs to be removed as this isn't a class. We can change the var keywords to "let" in the delcaration of lenA and lenB as these values do not change for the entireity of the function. Finally, I substitute "_" for letter in (letter, count) in the final for loop as the "letter" immutable variable is never used. I also return true at the end to satisfy the function declaration of boolean.
     
     
 }
